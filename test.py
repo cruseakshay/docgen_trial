41eae2ab0e077a87c131ef584edf6bdaa86cdb4e
Akshaykumar Salunke <cruseakshay@gmail.com>
2023-07-28 16:54:12 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+def pow(a, b):
+    return a**b
+
+
+def mul(a, b):
+    return a * b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mod(a, b):
+    return a % b
+
+
+def customFunc(a, b):
+    return (a * 3) + (b**0.5) + 8
