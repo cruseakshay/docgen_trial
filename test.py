5d59879b3edb3958e4042df19c3a216a101e6425
GitHub Action <action@github.com>
2023-07-29 07:09:48 +0000

    Add docstrings to .py files

--- test.py
+++ test.py
@@ -69,4 +69,13 @@ def customFunc(a, b):
 
 
 def func1(p_list):
+    """
+    Calculates the sum of all even numbers in a given list.
+
+    Args:
+        p_list (list): A list of integers.
+
+    Returns:
+        int: The sum of all even numbers in the list.
+    """
     return sum([ele for ele in p_list if ele % 2 == 0])
