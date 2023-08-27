5bca53dca3114caa7d0ab66eccc12c2ba552a90e
Akshaykumar Salunke <cruseakshay@gmail.com>
2023-08-27 23:08:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,22 @@ def sum_even(p_list):
         int: The sum of all even numbers in the list.
     """
     return sum([ele for ele in p_list if ele % 2 == 0])
+
+def pow(a, b):
+    return a**b
+
+
+def mul(a, b):
+    return a * b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mod(a, b):
+    return a % b
+
+
+def customFunc(a, b):
+    return (a * 3) + (b**0.5) + 8
