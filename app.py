0a10ff3379eb3594845826c8235e22b54f633cae
GitHub Action <action@github.com>
2023-08-27 17:44:34 +0000

    Add docstrings to .py files

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ TOKEN = "Your API TOKEN from BotFather"
 
 
 def imageAsDict(imageURL, caption):
+    """
+    Creates a dictionary representation of an image with its URL and caption.
+
+    Args:
+        imageURL (str): The URL of the image.
+        caption (str): The caption for the image.
+
+    Returns:
+        dict: A dictionary representing the image with its URL and caption.
+    """
     return {
         "type": "photo",
         "media": imageURL,
@@ -17,6 +27,16 @@ def imageAsDict(imageURL, caption):
 
 
 def sendMediaGroup(chatid, allImages):
+    """
+    Sends a group of media (images) to a Telegram chat.
+
+    Args:
+        chatid (int): The ID of the chat where the media will be sent.
+        allImages (list): A list of dictionaries containing the source URL and prompt for each image.
+
+    Returns:
+        requests.Response: The response object from the API call.
+    """
     url = f"https://api.telegram.org/bot{TOKEN}/sendMediaGroup"
     media = [imageAsDict(allImages[i]["src"], allImages[i]["prompt"]) for i in range(5)]
     payload = {"chat_id": chatid, "media": media}
@@ -25,6 +45,16 @@ def sendMediaGroup(chatid, allImages):
 
 
 def sendMessage(chat_id, text):
+    """
+    Sends a text message to a Telegram chat.
+
+    Args:
+        chat_id (int): The ID of the chat where the message will be sent.
+        text (str): The text of the message.
+
+    Returns:
+        requests.Response: The response object from the API call.
+    """
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": chat_id, "text": text}
     r = requests.post(url, json=payload)
@@ -33,6 +63,12 @@ def sendMessage(chat_id, text):
 
 @app.post("/")
 def index():
+    """
+    Handles the index route of the web app.
+
+    Returns:
+        Response: A response object with a status code of 200 indicating a successful request.
+    """
     msg = request.get_json()
     chat_id = msg["message"]["chat"]["id"]
     inputText = msg["message"]["text"]
--- test.py
+++ test.py
@@ -10,21 +10,72 @@ def sum_even(p_list):
     """
     return sum([ele for ele in p_list if ele % 2 == 0])
 
+
 def pow(a, b):
+    """
+    Calculates the power of a number.
+
+    Args:
+        a (int or float): The base number.
+        b (int or float): The exponent.
+
+    Returns:
+        int or float: The result of raising `a` to the power of `b`.
+    """
     return a**b
 
 
 def mul(a, b):
+    """
+    Multiplies two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The product of `a` and `b`.
+    """
     return a * b
 
 
 def sub(a, b):
+    """
+    Subtracts two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The difference between `a` and `b`.
+    """
     return a - b
 
 
 def mod(a, b):
+    """
+    Calculates the modulo of two numbers.
+
+    Args:
+        a (int or float): The dividend.
+        b (int or float): The divisor.
+
+    Returns:
+        int or float: The remainder of dividing `a` by `b`.
+    """
     return a % b
 
 
 def customFunc(a, b):
+    """
+    Performs a custom calculation on two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The result of the custom calculation, which is obtained by multiplying `a` by 3, adding the square root of `b`, and then adding 8.
+    """
     return (a * 3) + (b**0.5) + 8
